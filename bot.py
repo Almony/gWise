2ed04f63606098a8f608ff9bb8a4409b926d410b
navapathi <andrewunko@gmail.com>
2025-05-29 18:59:11 +0300

    Add asyncio console mode

--- bot.py
+++ bot.py
@@ -1,10 +1,17 @@
-# gWise/bot.py
-
+import sys
 import asyncio
+import argparse
+
+if sys.platform.startswith("linux"):
+    import uvloop
+    uvloop.install()
+
 from core.system.bot_context import BotContext
 from core.system.event_router import register_all
+from aioconsole import start_interactive_shell
 
-async def main():
+
+async def start_bot():
     BotContext.init()
     await BotContext.async_init()
 
@@ -17,6 +24,37 @@ async def main():
     async with app:
         logger.info("gWise bot is running...")
         await asyncio.Event().wait()
+
+
+async def open_console():
+    from aioconsole import start_interactive_shell
+
+    banner = "Async gWise Console — `ctx`, `db`, `bot`, `logger`"
+    print(banner)
+
+    await start_interactive_shell(
+        locals={
+            "bot": BotContext.bot,
+            "db": BotContext.db,
+            "logger": BotContext.logger,
+            "ctx": BotContext,
+        }
+    )
+
+
+
+async def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--console", action="store_true", help="Open async console after bot start")
+    args = parser.parse_args()
+
+    bot_task = asyncio.create_task(start_bot())
+
+    if args.console:
+        await open_console()
+    else:
+        await bot_task
+
 
 if __name__ == "__main__":
     asyncio.run(main())
